13ac3e01ca56b9ec5ffed2b6307d482bfbdae2b5
sc <sc@host.docker.internal>
2023-12-03 07:47:02 +0530

    this is first commit from eclipse

---
+++ src/main/java/mvn_jnkins/mvn_jenkins/App.java
@@ -0,0 +1,14 @@
+package mvn_jnkins.mvn_jenkins;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "application starting . " );
+    }
+    
+}
